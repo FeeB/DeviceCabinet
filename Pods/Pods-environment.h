254c0523e0e443c44c7484c16fab376589ff8523
FeeB <s0538335@htw-berlin.de>
2014-08-19 20:29:45 +0200

    Added CocoaPods + KeyItemWrapper to project

---
+++ Pods/Pods-environment.h
@@ -0,0 +1,14 @@
+
+// To check if a library is compiled with CocoaPods you
+// can use the `COCOAPODS` macro definition which is
+// defined in the xcconfigs so it is available in
+// headers also when they are imported in the client
+// project.
+
+
+// KeychainItemWrapper
+#define COCOAPODS_POD_AVAILABLE_KeychainItemWrapper
+#define COCOAPODS_VERSION_MAJOR_KeychainItemWrapper 1
+#define COCOAPODS_VERSION_MINOR_KeychainItemWrapper 2
+#define COCOAPODS_VERSION_PATCH_KeychainItemWrapper 0
+
