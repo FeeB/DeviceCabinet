9319cd0d838962362abacf50cc27d8b847786609
FeeB <s0538335@htw-berlin.de>
2014-09-14 19:34:51 +0100

    Removed commented out code

--- ApiExtension.h
+++ ApiExtension.h
@@ -13,15 +13,4 @@
 
 @interface ApiExtension : NSObject <DeviceCabinetDAO>
 
-//- (void)storeDevice:(NSDictionary *)deviceJson completionHandler:(void (^)(NSError *error))completionHandler;
-//- (void)fetchAllDevices:(void (^)(NSArray *deviceObjects, NSError *error))completionHandler;
-//- (void)fetchDeviceWithDeviceId:(NSString *)deviceId completionHandler:(void (^)(Device *device, NSError *error))completionHandler;
-//- (void)fetchDevicesWithPersonId:(NSInteger *)personRecordId completionHandler:(void (^)(NSArray *deviceObjects, NSError *error))completionHandler;
-//
-//- (void)storePerson:(NSDictionary *)personJson completionHandler:(void (^)(NSError *error))completionHandler;
-//- (void)fetchPersonWithUsername:(NSString *)username completionHandler:(void (^)(Person *person, NSError *error))completionHandler;
-//
-//- (void)storeReferenceToBookedDeviceWithDeviceId:(int *)recordDeviceId personId:(int *)recordPersonId completionHandler:(void (^)(NSError *error))completionHandler;
-//- (void)deleteReferenceFromBookedDeviceWithDeviceId:(int *)deviceId personId:(int *)personId completionHandler:(void (^)(NSError *error))completionHandler;
-
 @end
--- CloudKitManager.h
+++ CloudKitManager.h
@@ -14,21 +14,4 @@
 
 @interface CloudKitManager : NSObject <DeviceCabinetDAO>
 
-@property (readonly) CKDatabase *publicDatabase;
-
-//- (void)fetchDevicesWithCompletionHandler:(void (^)(NSArray *deviceObjects, NSError *error))completionHandler;
-//- (void)fetchDevicesWithDeviceName:(NSString *)deviceName completionHandler:(void (^)(NSArray *deviceObjects, NSError *error))completionHandler;
-//- (void)fetchDeviceWithDeviceId:(NSString *)deviceId completionHandler:(void (^)(Device *device, NSError *error))completionHandler;
-//- (void)fetchDevicesWithPerson:(Person *)person completionHandler:(void (^)(NSArray *devicesArray, NSError *error))completionHandler;
-//- (void)fetchDeviceRecordWithDevice:(Device *)device completionHandler:(void (^)(Device *device, NSError *error))completionHandler;
-//- (void)storeDevice:(Device *)device completionHandler:(void (^)(NSError *error))completionHandler;
-//
-//- (void)fetchPersonWithUsername:(NSString *)userName completionHandler:(void (^)(Person *person, NSError *error))completionHandler;
-//- (void)storePersonObjectAsReferenceWithDevice:(Device *)device person:(Person *)person completionHandler:(void (^)(CKRecord *record, NSError *error))completionHandler;
-//- (void)storePerson:(Person *)person completionHandler:(void (^)(NSError *error))completionHandler;
-//
-//- (void)deleteReferenceInDeviceWithDevice:(Device *)device completionHandler:(void (^)(CKRecord *record, NSError *error))completionHandler;
-//
-//- (void)uploadAssetWithURL:(NSURL *)assetURL device:(Device *)device completionHandler:(void (^)(Device *device, NSError *error))completionHandler;
-
 @end
